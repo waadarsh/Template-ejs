eafabcb13bb9c2144d3573f3f733c62a3dfcbce4
Aadarsh Unni Wilson <waadarsh@outlook.com>
2022-10-18 09:34:48 +0530

    minor

--- index.js
+++ index.js
@@ -3,10 +3,22 @@ app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
 const { response } = require('express');
-const pgp = require('pg-promise');
+const pgp = require('pg-promise')();
+
+const cn = {
+    user: 'postgres',
+    host: 'localhost',
+    database: 'rnaipl',
+    port: 5432,
+    password: 'nissan'
+};
+
+const db = pgp(cn);
 
 app.listen(3000);
 console.log('Listening on port 3000');
+
+app.use(bodyParser.text());
 
 app.use("/static", express.static("static"));
 app.set("view engine", "ejs");
@@ -26,11 +38,19 @@ app.get('/', (req, res) => {
 
     var chkdta = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ";
 
+    db.any("SELECT chklst_id,chklst_name,station_name,total_no_instruction FROM chklst_hdr WHERE status_code = 90;")
+    .then((data) => {
+        template_data=data;
+         res.render("index", template_data);////{
+        //     // data : data,
+        //     // hdr : hdr,
+        //     // bdy : bdy,
+        //     // chndta : chkdta
+        // });
+    }).catch(error => res.send(error));
+});
 
-    res.render('index', {
-        hdr : hdr,
-        bdy : bdy,
-        chndta : chkdta
-
-    });
+app.post("/", function(req, res) {
+    let data = req.body;
+    console.log(data);
 });
